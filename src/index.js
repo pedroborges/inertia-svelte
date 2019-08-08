c84a4a200666ae8f4ffb9728a9fe69e9d8e09d92
Pedro Borges <oi@pedroborg.es>
2019-08-07 23:34:03 -0300

    Replace pageProps with page

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import Inertia from 'inertia'
 import App from './App.svelte'
 import InertiaLink from './Link.svelte'
 import page from './page'
-import pageProps from './pageProps'
 import remember from './remember'
 
 export default App
-export { Inertia, InertiaLink, page, pageProps, remember }
+export { Inertia, InertiaLink, page, remember }
--- src/pageProps.js
+++
@@ -1,6 +0,0 @@
-import { derived } from 'svelte/store'
-import page from './page'
-
-const pageProps = derived(page, $page => $page.props)
-
-export default pageProps
